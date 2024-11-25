ddaca4067f976a6cdaf8aa56bedcdfcd8d938262
Nikita <selderui@gmail.com>
2024-11-25 20:02:48 +0300

    Тупик в запуске томката

--- JavaModule08/src/main/java/org/website/servlets/AuthServlet.java
+++ JavaModule08/src/main/java/org/website/servlets/AuthServlet.java
@@ -16,6 +16,11 @@ public class AuthServlet extends HttpServlet {
     private UserDAO userDAO = new UserDAO();
 
     @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("login.jsp").forward(req, resp);
+    }
+
+    @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
